09f52a5bda09b350cdaadcd29af73b9729466a8f
Irfan TUF <ammar.irf4n@gmail.com>
2025-09-15 17:50:04 +0800

    describe what you changed

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -3,46 +3,36 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.substitutions import LaunchConfiguration, Command, PathJoinSubstitution, FindExecutable
+from launch.substitutions import LaunchConfiguration, Command
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
-from launch_ros.parameter_descriptions import ParameterValue
+
+import xacro
 
 
 def generate_launch_description():
 
-    # Launch arguments
+    # Check if we're told to use sim time
     use_sim_time = LaunchConfiguration('use_sim_time')
     use_ros2_control = LaunchConfiguration('use_ros2_control')
 
-    # Locate xacro file
-    pkg_path = get_package_share_directory('protobot')
-    xacro_file = os.path.join(pkg_path, 'description', 'robot.urdf.xacro')
-
-    # Run xacro and make sure output is passed as a string
-    robot_description_config = ParameterValue(
-        Command([
-            FindExecutable(name='xacro'),
-            ' ',
-            xacro_file,
-            ' use_ros2_control:=', use_ros2_control,
-            ' sim_mode:=', use_sim_time
-        ]),
-        value_type=str
-    )
-
-    # robot_state_publisher node
+    # Process the URDF file
+    pkg_path = os.path.join(get_package_share_directory('protobot'))
+    xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
+    # robot_description_config = xacro.process_file(xacro_file).toxml()
+    robot_description_config = Command(['xacro ', xacro_file, ' use_ros2_control:=', use_ros2_control, ' sim_mode:=', use_sim_time])
+    
+    # Create a robot_state_publisher node
+    params = {'robot_description': robot_description_config, 'use_sim_time': use_sim_time}
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         output='screen',
-        parameters=[{
-            'robot_description': robot_description_config,
-            'use_sim_time': use_sim_time
-        }]
+        parameters=[params]
     )
 
-    # Launch description
+
+    # Launch!
     return LaunchDescription([
         DeclareLaunchArgument(
             'use_sim_time',
@@ -52,5 +42,6 @@ def generate_launch_description():
             'use_ros2_control',
             default_value='true',
             description='Use ros2_control if true'),
+
         node_robot_state_publisher
     ])
