4ea9ff4ab93c7d7abd6f248c4d97395dac461dc5
Muhamad Ammar Irfan <ammar.irf4n@gmail.com>
2026-04-04 09:53:51 +0800

    Add files via upload

---
+++ launch/paper_weight_serial.lauch.py
@@ -0,0 +1,17 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        DeclareLaunchArgument('use_sim_time', default_value='true'),
+
+        Node(
+            package='protobot',
+            executable='paper_weight_serial.py',  # NAMA FAIL BETUL
+            name='paper_weight_serial',
+            output='screen',
+            parameters=[{'use_sim_time': LaunchConfiguration('use_sim_time')}]
+        )
+    ])
